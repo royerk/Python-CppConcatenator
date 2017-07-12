11cc3929c13fb15f162543643e7d601890f7bfd2
Lysk <kroyer.github@gmail.com>
2017-07-12 12:04:01 +0200

    v0 functionnal, improved readme

--- README.md
+++ README.md
@@ -3,6 +3,15 @@ Python script to concatenate cpp files into a single file.
 
 **WIP**
 
+# How to
+
+Clone depot, in `cpp_concatenator.py` update paths to your project's folder. Run `python3 cpp_concatenator.py` to merge project to a single file. Use `python3 cpp_concatenator.py | xsel -b` to merge & copy to clipboard (you need to have `xsel` installed).
+
 # To do:
 - read class or structure from .h(pp)
 - regroup class/struct with .cpp stuff in the merged file
+
+# See also
+- [Go concatenator](https://github.com/royerk/Python-GoConcatenator)
+- [Js Cpp concatenator](https://github.com/wimgoeman/codingame-cpp-merge)
+- Java [Chelper](https://stackoverflow.com/questions/38492050/using-chelper-plugin-in-intellij-for-a-coding-contest) plugin for IntelliJ
--- cpp_concatenator.py
+++ cpp_concatenator.py
@@ -3,11 +3,33 @@ import os  # to get files names from a directory
 import sys  # for python script arguments
 import datetime # date in file name for versioning
 
+def find_class_struct(file_name, path_files="./", verbose=True):
+    """Find class or struct from a .hpp/.h file"""
+    result = []
+    with open(path_files+file_name) as fp:              # open file
+        record_lines = False
+        for line in fp:
+            if record_lines: # recording class lines
+                result.append(line)
+            details = line.split(" ")
+            if "class" in details or "struct" in details: # start recording
+                record_lines = True
+                result.append(line)
+            if line == "};\n": # end recording
+                record_lines = False
+    return result
+
+def find_all_class_struct(files_names, path_files="./", verbose=True):
+    result = []
+    for name in files_names:
+        if ".h" in name:
+            result.extend(find_class_struct(name, path_files=path_files, verbose=verbose))
+    return result
+
 def find_namespace(file_name, path_files="./", verbose=True):
     """Find imported packages from a .go file"""
     result = []
     with open(path_files+file_name) as fp:              # open file
-        read_in_next_line = False
         for line in fp:
             details = line.split(" ")
             if len(details) >= 3 and "using" in details[0] and "namespace" in details[1]:
@@ -52,23 +74,20 @@ def file_with_main(files_names, path_files="./"):
     return ""
 
 def extract_lines_no_imports(file):
-    """ Extract lines from a file, except includes"""
+    """ Extract lines from a file, except includes and namespaces"""
     skip_next = False
     lines = []
     for line in file:
-        if skip_next:                  # skip the imports
-            skip_next = line != ")\n"
+        details = line.split(" ")
+        if "#include" in details:   # skip include
+            pass
+        elif "namespace" in details: # skip namespace
+            pass
         else:
-            details = line.split(" ")
-            if "#include" in details:   # skip package declaration
-                pass
-            elif "namespace" in details:
-                pass
-            else:
-                lines.append(line)
+            lines.append(line)
     return lines
 
-def glue_in_one_list(files, imports, file_main, path_files, namespaces):
+def glue_in_one_list(files, imports, file_main, path_files, namespaces, class_struct):
     """ Create a list with every lines contained in the files"""
     blob = []
     for include in imports:
@@ -77,9 +96,14 @@ def glue_in_one_list(files, imports, file_main, path_files, namespaces):
     for namespace in namespaces:
         blob.append("using namespace "+namespace+";\n")
 
+    for foo in class_struct: # for each class or struct
+        for line in foo: # add each line
+            blob.append(line)
+        blob.append("\n")
+
     [blob.append(line) for line in extract_lines_no_imports(open(path_files+file_main))]  # file with main() goes first (arbitrary choice)
     for name in files:
-        [blob.append(line) for line in extract_lines_no_imports(open(path_files+name)) if name != file_main]  # the other files
+        [blob.append(line) for line in extract_lines_no_imports(open(path_files+name)) if name != file_main and not ".h" in name]  # the other files
 
     return blob
 
@@ -94,9 +118,9 @@ def cpp_files_list(path):
     files = []
     for (dirpath, dirnames, filenames) in os.walk(path):
         if dirpath ==path: # remove this to explore recursively
-            print(dirpath, dirnames, filenames)
+            #print(dirpath, dirnames, filenames)
             files.extend(filenames)
-    return [name for name in files if ".cpp" in name]
+    return [name for name in files if ".cpp" in name or ".h" in name or ".hpp" in name]
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
@@ -104,8 +128,8 @@ if __name__ == '__main__':
         path_output = sys.argv[1]
         file_output = sys.argv[2]
     else:
-        path_to_files = "./folder_with_cpp_files/"
-        path_output = "./versions/"
+        path_to_files = "../CG_VoxCodei/clion_prj/"
+        path_output = "../CG_VoxCodei/versions/"
         d = datetime.datetime.now()
         file_output = "current_"+d.strftime('%Y-%m-%d_%H-%M')+".cpp"
     print("//path_to_files:", path_to_files)
@@ -116,10 +140,11 @@ if __name__ == '__main__':
     files = cpp_files_list(path_to_files)
     print(files)
     includes = find_all_includes(files, path_files=path_to_files, verbose=False)
+    class_and_structs = find_all_class_struct(files, path_files=path_to_files, verbose=False)
     namespaces = find_all_namespaces(files, path_files=path_to_files, verbose=False)
     file_main = file_with_main(files, path_files=path_to_files)
     #print("//main() in:", file_main)
-    all_lines = glue_in_one_list(files, imports=includes, file_main=file_main, path_files=path_to_files, namespaces=namespaces)
+    all_lines = glue_in_one_list(files, imports=includes, file_main=file_main, path_files=path_to_files, namespaces=namespaces, class_struct=class_and_structs)
     list_to_file(all_lines, path_output=path_output, file_output=file_output)
 
     [print(line) for line in all_lines]
